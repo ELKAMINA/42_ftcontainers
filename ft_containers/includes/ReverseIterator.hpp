9dbb729cb21310b3b8b140dde50a7871d80a0b2c
elkamina@yahoo.com <ael-khat@e3r9p12.clusters.42paris.fr>
2022-12-19 21:47:01 +0100

    Oki avancement vector

--- ft_containers/includes/ReverseIterator.hpp
+++ ft_containers/includes/ReverseIterator.hpp
@@ -10,7 +10,8 @@ namespace ft
     {
         public:
             /* Shorthands and definitions */
-			typedef 		 ft::iterator<std::random_access_iterator_tag, T>  		standard;
+			typedef 		ft::iterator_traits<T>  								standard;
+			typedef			T														iterator_type;				
             typedef typename standard::difference_type      						difference_type;
             typedef typename standard::value_type           						value_type;
             typedef typename standard::pointer              						pointer;
@@ -18,39 +19,39 @@ namespace ft
             typedef typename standard::iterator_category    						iterator_category;
 
             /* Constructors and Destructors */
-            RandomAccessIterator() : _ptr(NULL) {}
-            RandomAccessIterator(pointer ptr) : _ptr(ptr) {}
-            RandomAccessIterator(const RandomAccessIterator<T> &other) : _ptr(other._ptr) {}
-            virtual ~RandomAccessIterator() {} // virtual destructor : why?
+            reverse_iterator() {}
+            reverse_iterator(pointer ptr) : _ptr(ptr) {}
+            reverse_iterator(const reverse_iterator<T> &other) : _ptr(other._ptr) {}
+            virtual ~reverse_iterator() {} // virtual destructor : why?
 
             /* Retrieve the current pointer */
             pointer base() const { return this->_ptr;};
             
             /* Comparison Operators */
-            RandomAccessIterator &operator=(const RandomAccessIterator &other)
+            reverse_iterator &operator=(const reverse_iterator &other)
             {
                 if (this != &other)
                     _ptr = other._ptr;
                 return *this;
             }
-            bool operator==(const RandomAccessIterator &other) const { return _ptr == other._ptr; } // OK
-            bool operator!=(const RandomAccessIterator &other) const { return _ptr != other._ptr; } // OK
-            bool operator<(const RandomAccessIterator &other) const { return _ptr < other._ptr; }
-            bool operator<=(const RandomAccessIterator &other) const { return _ptr <= other._ptr; }
-            bool operator>(const RandomAccessIterator &other) const { return _ptr > other._ptr; }
-            bool operator>=(const RandomAccessIterator &other) const { return _ptr >= other._ptr; }
+            bool operator==(const reverse_iterator &other) const { return _ptr == other._ptr; } // OK
+            bool operator!=(const reverse_iterator &other) const { return _ptr != other._ptr; } // OK
+            bool operator<(const reverse_iterator &other) const { return _ptr < other._ptr; }
+            bool operator<=(const reverse_iterator &other) const { return _ptr <= other._ptr; }
+            bool operator>(const reverse_iterator &other) const { return _ptr > other._ptr; }
+            bool operator>=(const reverse_iterator &other) const { return _ptr >= other._ptr; }
 
             /* Operators */
             reference operator*() const {return *_ptr; } // OK
             pointer operator->() const { return _ptr; } // OK
-            RandomAccessIterator &operator++() { ++_ptr; return *this; } // OK
-            RandomAccessIterator operator++(int) { RandomAccessIterator tmp(*this); ++_ptr; return tmp; } // OK
-            RandomAccessIterator &operator--() { --_ptr; return *this; } // OK prefix -- 
-            RandomAccessIterator operator--(int) { RandomAccessIterator tmp(*this); --_ptr; return tmp; } // OK postfix --
-            RandomAccessIterator operator+(difference_type n) const { return RandomAccessIterator(_ptr + n); } // OK
-            RandomAccessIterator operator-(difference_type n) const { return RandomAccessIterator(_ptr - n); } // OK
-            RandomAccessIterator &operator+=(difference_type n) { _ptr += n; return *this; } //OK arithmetic op between iterator and int value/another it
-            RandomAccessIterator &operator-=(difference_type n) { _ptr -= n; return;}// OK arithmetic op between iterator and int value/another it
+            reverse_iterator &operator++() { ++_ptr; return *this; } // OK
+            reverse_iterator operator++(int) { reverse_iterator tmp(*this); ++_ptr; return tmp; } // OK
+            reverse_iterator &operator--() { --_ptr; return *this; } // OK prefix -- 
+            reverse_iterator operator--(int) { reverse_iterator tmp(*this); --_ptr; return tmp; } // OK postfix --
+            reverse_iterator operator+(difference_type n) const { return reverse_iterator(_ptr + n); } // OK
+            reverse_iterator operator-(difference_type n) const { return reverse_iterator(_ptr - n); } // OK
+            reverse_iterator &operator+=(difference_type n) { _ptr += n; return *this; } //OK arithmetic op between iterator and int value/another it
+            reverse_iterator &operator-=(difference_type n) { _ptr -= n; return;}// OK arithmetic op between iterator and int value/another it
             reference operator[](difference_type n) const { return _ptr[n]; } //OK
 
         	private:
@@ -60,64 +61,65 @@ namespace ft
 
         /* Template specialisations for operators - Different cases */
         template <typename T>
-        inline bool operator==(const ft::RandomAccessIterator<T> &lhs, const ft::RandomAccessIterator<T> &rhs)
+        inline bool operator==(const ft::reverse_iterator<T> &lhs, const ft::reverse_iterator<T> &rhs)
         { return lhs.base() == rhs.base(); }
 
         template<typename T1, typename T2>
-        inline bool operator==(const ft::RandomAccessIterator<T1> &lhs, const ft::RandomAccessIterator<T2> &rhs)
+        inline bool operator==(const ft::reverse_iterator<T1> &lhs, const ft::reverse_iterator<T2> &rhs)
         { return lhs.base() == rhs.base(); }
 
         template <typename T>
-        inline bool operator!=(const ft::RandomAccessIterator<T> &lhs, const ft::RandomAccessIterator<T> &rhs)
+        inline bool operator!=(const ft::reverse_iterator<T> &lhs, const ft::reverse_iterator<T> &rhs)
         { return lhs.base() != rhs.base(); }
 
         template<typename T1, typename T2>
-        inline bool operator!=(const ft::RandomAccessIterator<T1> &lhs, const ft::RandomAccessIterator<T2> &rhs)
+        inline bool operator!=(const ft::reverse_iterator<T1> &lhs, const ft::reverse_iterator<T2> &rhs)
         { return lhs.base() != rhs.base(); }
 
         template <typename T>
-        inline bool operator<(const ft::RandomAccessIterator<T> &lhs, const ft::RandomAccessIterator<T> &rhs)
+        inline bool operator<(const ft::reverse_iterator<T> &lhs, const ft::reverse_iterator<T> &rhs)
         { return lhs.base() < rhs.base(); }
 
         template<typename T1, typename T2>
-        inline bool operator<(const ft::RandomAccessIterator<T1> &lhs, const ft::RandomAccessIterator<T2> &rhs)
+        inline bool operator<(const ft::reverse_iterator<T1> &lhs, const ft::reverse_iterator<T2> &rhs)
         { return lhs.base() < rhs.base(); }
 
         template <typename T>
-        inline bool operator>(const ft::RandomAccessIterator<T> &lhs, const ft::RandomAccessIterator<T> &rhs)
+        inline bool operator>(const ft::reverse_iterator<T> &lhs, const ft::reverse_iterator<T> &rhs)
         { return lhs.base() > rhs.base(); }
 
         template<typename T1, typename T2>
-        inline bool operator>(const ft::RandomAccessIterator<T1> &lhs, const ft::RandomAccessIterator<T2> &rhs)
+        inline bool operator>(const ft::reverse_iterator<T1> &lhs, const ft::reverse_iterator<T2> &rhs)
         { return lhs.base() > rhs.base(); }
 
         template <typename T>
-        inline bool operator<=(const ft::RandomAccessIterator<T> &lhs, const ft::RandomAccessIterator<T> &rhs)
+        inline bool operator<=(const ft::reverse_iterator<T> &lhs, const ft::reverse_iterator<T> &rhs)
         { return lhs.base() <= rhs.base(); }
 
         template<typename T1, typename T2>
-        inline bool operator<=(const ft::RandomAccessIterator<T1> &lhs,
-                const ft::RandomAccessIterator<T2> &rhs)
+        inline bool operator<=(const ft::reverse_iterator<T1> &lhs,
+                const ft::reverse_iterator<T2> &rhs)
         { return lhs.base() <= rhs.base(); }
 
         template <typename T>
-        inline bool operator>=(const ft::RandomAccessIterator<T> &lhs,
-                const ft::RandomAccessIterator<T> &rhs)
+        inline bool operator>=(const ft::reverse_iterator<T> &lhs,
+                const ft::reverse_iterator<T> &rhs)
         { return lhs.base() >= rhs.base(); }
 
         template<typename T1, typename T2>
-        inline bool operator>=(const ft::RandomAccessIterator<T1> &lhs, const ft::RandomAccessIterator<T2> &rhs)
+        inline bool operator>=(const ft::reverse_iterator<T1> &lhs, const ft::reverse_iterator<T2> &rhs)
         { return lhs.base() >= rhs.base(); }
 
         template<typename T>
-        inline ft::RandomAccessIterator<T> operator+(typename ft::RandomAccessIterator<T>::difference_type n,typename ft::RandomAccessIterator<T> it)
+        inline ft::reverse_iterator<T> operator+(typename ft::reverse_iterator<T>::difference_type n,typename ft::reverse_iterator<T> it)
         { return &(*it) + n; }
 
         template <typename T>
-        inline typename ft::RandomAccessIterator<T>::difference_type operator-(const ft::RandomAccessIterator<T> &lhs, const ft::RandomAccessIterator<T> &rhs)
+        inline typename ft::reverse_iterator<T>::difference_type operator-(const ft::reverse_iterator<T> &lhs, const ft::reverse_iterator<T> &rhs)
         { return lhs.base() - rhs.base(); }
 
         template<typename T1, typename T2>
-        inline typename ft::RandomAccessIterator<T1>::difference_type operator-(const ft::RandomAccessIterator<T1> &lhs, const ft::RandomAccessIterator<T2> &rhs)
+        inline typename ft::reverse_iterator<T1>::difference_type operator-(const ft::reverse_iterator<T1> &lhs, const ft::reverse_iterator<T2> &rhs)
         { return lhs.base() - rhs.base(); }
 } 
+                                                         
